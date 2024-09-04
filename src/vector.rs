a84cd60e98cd4758214503574e12a2931a2c94a1
xyzBits <lidf077@gmail.com>
2024-09-04 20:49:27 +0800

    dot product

--- src/vector.rs
+++ src/vector.rs
@@ -1,9 +1,25 @@
-use std::ops::Deref;
+use anyhow::{anyhow, Result};
+use std::ops::{Add, AddAssign, Deref, Mul};
 
 pub struct Vector<T> {
     data: Vec<T>,
 }
 
+pub fn dot_product<T>(a: Vector<T>, b: Vector<T>) -> Result<T>
+where
+    T: Copy + Default + Add<Output=T> + AddAssign + Mul<Output=T>,
+{
+    if a.len() != b.len() {
+        return Err(anyhow!("Dot product error: a.len != b.len"));
+    }
+
+    let mut sum = T::default();
+    for i in 0..a.len() {
+        sum += a[i] * b[i]
+    }
+
+    Ok(sum)
+}
 impl<T> Deref for Vector<T> {
     type Target = Vec<T>;
 
