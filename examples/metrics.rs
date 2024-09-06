306aebb96b7c5a65961e9e80eb20d4815003d873
xyzBits <lidf077@gmail.com>
2024-09-06 15:58:14 +0800

    metrics support RWLock

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -20,7 +20,7 @@ fn main() -> Result<()> {
 
     loop {
         thread::sleep(Duration::from_secs(5));
-        println!("{:?}", metrics.snapshot());
+        println!("{}", metrics);
     }
 }
 
--- src/metrics.rs
+++ src/metrics.rs
@@ -1,18 +1,19 @@
 use anyhow::{anyhow, Result};
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::fmt::Display;
+use std::sync::{Arc, RwLock};
 
 /// metrics 的数据结构
 /// 如果加了 clone ，clone后，就完全是一个新的 metrics
 #[derive(Debug, Clone)] // clone 是对 Arc 进行 clone
 pub struct Metrics {
-    data: Arc<Mutex<HashMap<String, i64>>>,
+    data: Arc<RwLock<HashMap<String, i64>>>,
 }
 
 impl Metrics {
     pub fn new() -> Self {
         Self {
-            data: Arc::new(Mutex::new(HashMap::new())),
+            data: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
@@ -22,7 +23,7 @@ impl Metrics {
         let mut mutex_map = self
             .data
             // lock()? 返回的错误无法在线程之间安全的传递
-            .lock()
+            .write()
             .map_err(|e| {
                 let error = anyhow!(e.to_string());
                 error
@@ -35,7 +36,7 @@ impl Metrics {
     }
 
     pub fn dec(&self, key: impl Into<String>) -> Result<()> {
-        let mut mutex_map = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let mut mutex_map = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
 
         let counter = mutex_map.entry(key.into()).or_insert(1);
         *counter -= 1;
@@ -46,8 +47,24 @@ impl Metrics {
     pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
         Ok(self
             .data
-            .lock()
+            .read()
             .map_err(|e| anyhow!(e.to_string()))?
             .clone())
+    }
+}
+
+impl Display for Metrics {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let data = self
+            .data
+            .read()
+            // error 无法转换，直接生成一个新的 fmt::Error
+            .map_err(|_e| std::fmt::Error {})?;
+
+        for (key, value) in data.iter() {
+            writeln!(f, "{}: {}", key, value)?;
+        }
+
+        Ok(())
     }
 }
