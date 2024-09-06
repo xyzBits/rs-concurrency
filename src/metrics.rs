cab78bd40103d50479e7c002e8fb31c333877660
xyzBits <lidf077@gmail.com>
2024-09-06 16:11:15 +0800

    dashmap metrics

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,68 +1,58 @@
-use anyhow::{anyhow, Result};
-use std::collections::HashMap;
+use anyhow::Result;
+use dashmap::DashMap;
 use std::fmt::Display;
-use std::sync::{Arc, RwLock};
+use std::sync::Arc;
 
 /// metrics 的数据结构
 /// 如果加了 clone ，clone后，就完全是一个新的 metrics
 #[derive(Debug, Clone)] // clone 是对 Arc 进行 clone
 pub struct Metrics {
-    data: Arc<RwLock<HashMap<String, i64>>>,
+    // 锁相关的操作，全部被封装在 DashMap 的内部
+    data: Arc<DashMap<String, i64>>,
 }
 
 impl Metrics {
     pub fn new() -> Self {
         Self {
-            data: Arc::new(RwLock::new(HashMap::new())),
+            data: Arc::new(DashMap::new()),
         }
     }
 
     // 任何东西，只要能够转换为 String 都可以作为 key
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
         // `MutexGuard<'_, HashMap<String, i64>>` cannot be sent between threads safely
-        let mut mutex_map = self
+        let mut counter = self
             .data
             // lock()? 返回的错误无法在线程之间安全的传递
-            .write()
-            .map_err(|e| {
-                let error = anyhow!(e.to_string());
-                error
-            })?;
+            .entry(key.into())
+            .or_insert(0);
 
-        let counter = mutex_map.entry(key.into()).or_insert(0);
         *counter += 1;
 
         Ok(())
     }
 
     pub fn dec(&self, key: impl Into<String>) -> Result<()> {
-        let mut mutex_map = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
+        let mut counter = self.data.entry(key.into()).or_insert(0);
 
-        let counter = mutex_map.entry(key.into()).or_insert(1);
         *counter -= 1;
 
         Ok(())
     }
 
-    pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
-        Ok(self
-            .data
-            .read()
-            .map_err(|e| anyhow!(e.to_string()))?
-            .clone())
-    }
+    // pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
+    //     Ok(self
+    //         .data
+    //         .read()
+    //         .map_err(|e| anyhow!(e.to_string()))?
+    //         .clone())
+    // }
 }
 
 impl Display for Metrics {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let data = self
-            .data
-            .read()
-            // error 无法转换，直接生成一个新的 fmt::Error
-            .map_err(|_e| std::fmt::Error {})?;
-
-        for (key, value) in data.iter() {
-            writeln!(f, "{}: {}", key, value)?;
+        for entry in self.data.iter() {
+            writeln!(f, "{}: {}", entry.key(), entry.value())?;
         }
 
         Ok(())
