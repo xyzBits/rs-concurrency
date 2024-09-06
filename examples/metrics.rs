0ab3ab293cf7bee98df59e4598b870494f0f2cf2
xyzBits <lidf077@gmail.com>
2024-09-06 11:59:03 +0800

    impl metrics concurrently

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -1,5 +1,11 @@
 use anyhow::Result;
+use rand::Rng;
 use rs_concurrency::Metrics;
+use std::thread;
+use std::time::Duration;
+
+const N: usize = 2;
+const M: usize = 2;
 
 fn main() -> Result<()> {
     let mut metrics = Metrics::new();
@@ -20,7 +26,35 @@ fn main() -> Result<()> {
         metrics.dec("req.page.2");
     }
 
+    for idx in 0..N {
+        task_worker(idx, metrics);
+    }
+
+    for _ in 0..M {
+        request_worker(metrics);
+    }
+
     println!("{:?}", metrics.snapshot());
 
     Ok(())
+}
+
+fn task_worker(idx: usize, mut metrics: Metrics) {
+    thread::spawn(move || loop {
+        // do something
+        let mut rng = rand::thread_rng();
+        thread::sleep(Duration::from_millis(rng.gen_range(100..5_000)));
+        metrics.inc(format!("call.thread.worker.{}", idx));
+    });
+}
+
+fn request_worker(mut metrics: Metrics) {
+    thread::spawn(move || loop {
+        let mut rng = rand::thread_rng();
+
+        thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+
+        let page = rng.gen_range(1..256);
+        metrics.inc(format!("req.page.{}", page));
+    });
 }
--- src/metrics.rs
+++ src/metrics.rs
@@ -1,29 +1,48 @@
+use anyhow::{anyhow, Result};
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
 /// metrics 的数据结构
+/// 如果加了 clone ，clone后，就完全是一个新的 metrics
 #[derive(Debug)]
 pub struct Metrics {
-    data: HashMap<String, i64>,
+    data: Arc<Mutex<HashMap<String, i64>>>,
 }
 
 impl Metrics {
     pub fn new() -> Self {
         Self {
-            data: HashMap::new(),
+            data: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 
-    pub fn inc(&mut self, key: impl Into<String>) {
-        let counter = self.data.entry(key.into()).or_insert(0);
+    // 任何东西，只要能够转换为 String 都可以作为 key
+    pub fn inc(&self, key: impl Into<String>) -> Result<()> {
+        // `MutexGuard<'_, HashMap<String, i64>>` cannot be sent between threads safely
+        let mut data = self
+            .data
+            // lock()? 返回的错误无法在线程之间安全的传递
+            .lock()
+            .map_err(|e| {
+                let error = anyhow!(e.to_string());
+                error
+            })?;
+
+        let counter = data.entry(key.into()).or_insert(0);
         *counter += 1;
+
+        Ok(())
     }
 
-    pub fn dec(&mut self, key: impl Into<String>) {
-        let counter = self.data.entry(key.into()).or_insert(0);
-        *counter -= 1;
+    pub fn dec(&mut self, key: impl Into<String>) -> Result<()> {
+        // let counter = self.data.entry(key.into()).or_insert(0);
+        // *counter -= 1;
+
+        Ok(())
     }
 
     pub fn snapshot(&self) -> HashMap<String, i64> {
-        self.data.clone()
+        // self.data.clone()
+        todo!()
     }
 }
